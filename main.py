d3de07bbc20f95e75728c2604566871cbccfaf2e
Haziq Hakimi <haziqhakimiyes@gmail.com>
2024-12-03 20:49:57 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,30 @@
+"""
+********************************************************************************
+* Project Name:  Counting Squirrel
+* Description:   This project illustrate how to extract information from a CSV file into a new CSV file
+* Author:        ziqkimi308
+* Created:       2024-12-03
+* Updated:       2024-12-03
+* Version:       1.0
+********************************************************************************
+"""
+
+import pandas
+
+# Read csv
+data = pandas.read_csv("2018-Central-Park-Squirrel-Census-Squirrel-Data.csv")
+
+# Specifies data
+grey_count = len(data[data["Primary Fur Color"] == "Gray"])
+red_count = len(data[data["Primary Fur Color"] == "Cinnamon"])
+black_count = len(data[data["Primary Fur Color"] == "Black"])
+
+# Create dictionary
+data_dict = {
+    "Fur Color" : ["Gray", "Cinnamon", "Black"],
+    "Count" : [grey_count, red_count, black_count]
+}
+
+# Convert dict to dataframe to csv
+dataframe = pandas.DataFrame(data_dict)
+dataframe.to_csv("squirrel_count.csv")
